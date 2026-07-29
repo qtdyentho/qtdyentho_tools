c2d249ee5e8cbea110b91fb8401c02b287694b1f
qtdyentho <qtdyentho@gmail.com>
2026-07-29 13:19:52 +0700

    Cleanup temporary v9 extraction scripts

--- extract_v9.ps1
+++
@@ -1,7 +0,0 @@
-$dir = Get-Location
-$file = Join-Path $dir "PWA_QTDYENTHO.html"
-$content = git show 74db889:PWA_QTDYENTHO.html | Out-String
-$match = [System.Text.RegularExpressions.Regex]::Match($content, "(?s)function renderStandeeCanvasToTarget.*?function renderQRLoaCanvasToTarget")
-if ($match.Success) {
-    Write-Host $match.Value.Substring(0, [Math]::Min(1200, $match.Value.Length))
-}
--- get_v9_code.ps1
+++
@@ -1,5 +0,0 @@
-$content = git show 74db889:PWA_QTDYENTHO.html
-$idx = $content.IndexOf("function renderStandeeCanvasToTarget")
-if ($idx -gt 0) {
-    Write-Host $content.Substring($idx, 1500)
-}
