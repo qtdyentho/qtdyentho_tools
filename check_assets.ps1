d81aaf4e4fda3a37a37ebbe3d06769bc3ac571ce
qtdyentho <qtdyentho@gmail.com>
2026-08-01 00:20:00 +0700

    CHORE: Dọn dẹp tệp tạm chẩn đoán

--- check_assets.ps1
+++
@@ -1,7 +0,0 @@
-$logoB64 = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes('LOgoQR.jpg'))
-$bgB64 = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes('BackgroundQR.png'))
-$loaB64 = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes('QRLOA.png'))
-
-Write-Host "LOgoQR.jpg B64 length:" $logoB64.Length
-Write-Host "BackgroundQR.png B64 length:" $bgB64.Length
-Write-Host "QRLOA.png B64 length:" $loaB64.Length
